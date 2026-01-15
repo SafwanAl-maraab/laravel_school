2fdc54e46eecc5f4c67ac1edff2380227ca5d24f
SafwanAl-maraab <sfwanalmrb@gmail.com>
2026-01-15 11:12:44 +0300

    add users route and page

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 
+
+use App\Http\Controllers\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
+
+
 Route::get('/', function () {
 
 
 
     return view('welcome');
-});
-
-
-Route::get('/view', function () {
-    return view('views');
-});
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 });
 
 
